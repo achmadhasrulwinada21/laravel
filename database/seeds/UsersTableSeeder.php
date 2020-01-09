20c4b27217f6bb1be4239424e87cf3bc3ffd20b5
achmad21 <achmadhasrulwinada@gmail.com>
2020-01-09 20:17:20 +0700

    test3

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,6 +16,7 @@ class UsersTableSeeder extends Seeder
         $user->name             = "admin";
         $user->password         = bcrypt("admin");
         $user->email            = "admin@mail.com";
+        $user->jabatan          ="admin";
         $user->save();
     }
 }
